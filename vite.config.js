5302e6604b89e185f6e9e52ac0245658bd35c714
lzzheng <lzzheng@cn.ibm.com>
2025-10-19 11:38:21 +0800

    add: vite config(src)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
